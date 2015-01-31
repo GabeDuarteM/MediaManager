4d67e832ee7a0a9826c00261e009d85144677027
Gabriel Duarte <gabrielqduarte@gmail.com>
2015-01-30 23:42:51 -0200

    Interface: Tela principal e de preferências prontas

---
+++ MediaManager/Forms/frmMain.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menuItAdicionarSerie_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void menuItAdicionarFilme_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void menuItAdicionarAnime_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void menuItProcurarConteudo_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void menuItRenomearTudo_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void menuItRenomearSerie_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void menuItRenomearFilmes_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void menuItRenomearAnimes_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void menuItPreferencias_Click(object sender, RoutedEventArgs e)
+        {
+            Forms.frmPreferencias frmPreferencias = new Forms.frmPreferencias();
+            frmPreferencias.ShowDialog();
+            if (frmPreferencias.DialogResult == true)
+            {
+                // @TODO Salvar preferencias
+            }
+        }
+
+        private void menuItSair_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
---
+++ MediaManager/Forms/frmPreferencias.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MediaManager.Forms
+{
+    /// <summary>
+    /// Interaction logic for frmPreferencias.xaml
+    /// </summary>
+    public partial class frmPreferencias : Window
+    {
+        public frmPreferencias()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectSerie_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnSelectFilme_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnSelectAnime_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnSelectDownloads_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnLoginTrakt_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
