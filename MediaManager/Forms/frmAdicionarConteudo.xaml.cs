475b6e22cf7a19073e8200fb808782d522fcda15
Gabriel Duarte <gabrielqduarte@gmail.com>
2015-02-01 05:06:08 -0200

    Interface: Ajuste - Tooltip nos botões de selecionar pasta

---
+++ MediaManager/Forms/frmAdicionarConteudo.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MediaManager.Forms
+{
+    /// <summary>
+    /// Interaction logic for frmAdicionarSerie.xaml
+    /// </summary>
+    public partial class frmAdicionarConteudo : Window
+    {
+        public frmAdicionarConteudo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConfig_Click(object sender, RoutedEventArgs e)
+        {
+            Forms.frmConfigConteudo frmConfigConteudo = new frmConfigConteudo();
+            frmConfigConteudo.ShowDialog();
+            if (frmConfigConteudo.DialogResult == true)
+            {
+                // @TODO Salvar configuração do conteúdo
+            }
+        }
+
+        private void btnPasta_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
