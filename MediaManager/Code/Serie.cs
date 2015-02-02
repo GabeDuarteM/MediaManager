2553514dcf2dd14f648652803ed5eb17a9d952a8
Gabriel Duarte <gabrielqduarte@gmail.com>
2015-02-02 20:27:07 +0000

    Updated Serie.cs

--- MediaManager/Code/Serie.cs
+++ MediaManager/Code/Serie.cs
@@ -7,108 +7,119 @@ using System.Threading.Tasks;
 
 namespace MediaManager.Code.Series
 {
-    public class Ids
-    {
-        [JsonProperty("trakt")]
-        public string trakt { get; set; }
+	public class Ids
+	{
+		[JsonProperty("trakt")]
+		public string trakt { get; set; }
 
-        [JsonProperty("slug")]
-        public string slug { get; set; }
+		[JsonProperty("slug")]
+		public string slug { get; set; }
 
-        [JsonProperty("tvdb")]
-        public string tvdb { get; set; }
+		[JsonProperty("tvdb")]
+		public string tvdb { get; set; }
 
-        [JsonProperty("imdb")]
-        public string imdb { get; set; }
+		[JsonProperty("imdb")]
+		public string imdb { get; set; }
 
-        [JsonProperty("tmdb")]
-        public string tmdb { get; set; }
+		[JsonProperty("tmdb")]
+		public string tmdb { get; set; }
 
-        [JsonProperty("tvrage")]
-        public string tvrage { get; set; }
-    }
+		[JsonProperty("tvrage")]
+		public string tvrage { get; set; }
+	}
 
-    public class Fanart
-    {
-        [JsonProperty("full")]
-        public string full { get; set; }
+	public class Fanart
+	{
+		[JsonProperty("full")]
+		public string full { get; set; }
 
-        [JsonProperty("medium")]
-        public string medium { get; set; }
+		[JsonProperty("medium")]
+		public string medium { get; set; }
 
-        [JsonProperty("thumb")]
-        public string thumb { get; set; }
-    }
+		[JsonProperty("thumb")]
+		public string thumb { get; set; }
+	}
 
-    public class Poster
-    {
-        [JsonProperty("full")]
-        public string full { get; set; }
+	public class Poster
+	{
+		[JsonProperty("full")]
+		public string full { get; set; }
 
-        [JsonProperty("medium")]
-        public string medium { get; set; }
+		[JsonProperty("medium")]
+		public string medium { get; set; }
 
-        [JsonProperty("thumb")]
-        public string thumb { get; set; }
-    }
+		[JsonProperty("thumb")]
+		public string thumb { get; set; }
+	}
 
-    public class Logo
-    {
-        [JsonProperty("full")]
-        public string full { get; set; }
-    }
+	public class Logo
+	{
+		[JsonProperty("full")]
+		public string full { get; set; }
+	}
 
-    public class Clearart
-    {
-        [JsonProperty("full")]
-        public string full { get; set; }
-    }
+	public class Clearart
+	{
+		[JsonProperty("full")]
+		public string full { get; set; }
+	}
 
-    public class Banner
-    {
-        [JsonProperty("full")]
-        public string full { get; set; }
-    }
+	public class Banner
+	{
+		[JsonProperty("full")]
+		public string full { get; set; }
+	}
 
-    public class Thumb
-    {
-        [JsonProperty("full")]
-        public string full { get; set; }
-    }
+	public class Thumb
+	{
+		[JsonProperty("full")]
+		public string full { get; set; }
+	}
 
-    public class Images
-    {
-        [JsonProperty("fanart")]
-        public Fanart fanart { get; set; }
+	public class Images
+	{
+		[JsonProperty("fanart")]
+		public Fanart fanart { get; set; }
 
-        [JsonProperty("poster")]
-        public Poster poster { get; set; }
+		[JsonProperty("poster")]
+		public Poster poster { get; set; }
 
-        [JsonProperty("logo")]
-        public Logo logo { get; set; }
+		[JsonProperty("logo")]
+		public Logo logo { get; set; }
 
-        [JsonProperty("clearart")]
-        public Clearart clearart { get; set; }
+		[JsonProperty("clearart")]
+		public Clearart clearart { get; set; }
 
-        [JsonProperty("banner")]
-        public Banner banner { get; set; }
+		[JsonProperty("banner")]
+		public Banner banner { get; set; }
 
-        [JsonProperty("thumb")]
-        public Thumb thumb { get; set; }
-    }
+		[JsonProperty("thumb")]
+		public Thumb thumb { get; set; }
+	}
 
-    public class Serie
-    {
+	public class Serie
+	{
+		[JsonProperty("title")]
+		public string title { get; set; }
+
+		[JsonProperty("year")]
+		public string year { get; set; }
+
+		[JsonProperty("ids")]
+		public Ids ids { get; set; }
+
+		[JsonProperty("images")]
+		public Images images { get; set; }
+        
+	}
+	
+	public class Translations {
+		
         [JsonProperty("title")]
         public string title { get; set; }
-
-        [JsonProperty("year")]
-        public string year { get; set; }
-
-        [JsonProperty("ids")]
-        public Ids ids { get; set; }
-
-        [JsonProperty("images")]
-        public Images images { get; set; }
-    }
+        [JsonProperty("overview")]
+        public string overview { get; set; }
+        [JsonProperty("language")]
+        public string language { get; set; }
+	}
 }
